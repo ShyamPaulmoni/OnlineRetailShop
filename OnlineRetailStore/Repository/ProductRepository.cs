d6f1c405f1fb07a9b17fe81ffac31ed61e3d26f2
Shyam Paulmoni <shyamjaya.paulmoni@cadsglobal.com>
2022-02-05 11:56:08 +0530

    removed sql references

--- OnlineRetailStore/Repository/ProductRepository.cs
+++ OnlineRetailStore/Repository/ProductRepository.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
 using OnlineRetailStore.Context;
 using OnlineRetailStore.Models;
--- OnlineRetailStore/Services/OrderServices.cs
+++ OnlineRetailStore/Services/OrderServices.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OnlineRetailStore.Models;
 using OnlineRetailStore.Models.Interfaces;
